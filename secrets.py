19cbf439192b97083a374844a18b42b7f905e439
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2021-04-19 22:24:10 -0700

    The Secret Password Hash

--- secrets.py
+++ secrets.py
@@ -1,2 +1,3 @@
 # For testing purposes, this is the hash for the password "password123" (no quotes)
+# NOTE: The following variable must exist in this file for the app to function
 password_answer = 'ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f'
