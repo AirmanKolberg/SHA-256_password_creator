c62f02e73477e3bb39f9112ca6e8ef5fc62f37ba
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2021-04-19 22:22:41 -0700

    Custom System Commands

--- system_functions.py
+++ system_functions.py
@@ -1,15 +1,13 @@
 from os import system
 
 
-def clear_screen():
-    _ = system('clear')
-
-
 def bash_command(user_in):
     _ = system(user_in)
 
 
+def clear_screen():
+    bash_command('clear')
+
+
 def save_new_test_hash(new_hash):
     bash_command(f"""rm secrets.py && echo "password_answer = '{new_hash}'" >> secrets.py""")
-
-
