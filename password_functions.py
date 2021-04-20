829030d428c7e793ebe6ee5959851d4944de0ba6
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2021-04-20 13:16:39 -0700

    Custom Password-Hashing Functions

--- password_functions.py
+++ password_functions.py
@@ -1,11 +1,11 @@
 import hashlib
 
 
-def verify_password(user_pass, correct_password):
-    user_pass_converted = bytes(user_pass, 'utf-8')
-    password_guess = hashlib.sha256(user_pass_converted).hexdigest()
+def verify_password(user_in_pass, correct_pass_hash):
+    user_in_converted = bytes(user_in_pass, 'utf-8')
+    password_guess = hashlib.sha256(user_in_converted).hexdigest()
 
-    if password_guess == correct_password:
+    if password_guess == correct_pass_hash:
         return True
     else:
         return False
