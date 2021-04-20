0045028d4c290d39ff5145aca005dda85e67892a
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2021-04-19 22:21:38 -0700

    All System Functions

--- system_functions.py
+++ system_functions.py
@@ -5,9 +5,11 @@ def clear_screen():
     _ = system('clear')
 
 
-def bash_input(user_in):
+def bash_command(user_in):
     _ = system(user_in)
 
 
 def save_new_test_hash(new_hash):
-    bash_input(f"""rm secrets.py && echo "password_answer = '{new_hash}'" >> secrets.py""")
+    bash_command(f"""rm secrets.py && echo "password_answer = '{new_hash}'" >> secrets.py""")
+
+
